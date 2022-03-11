9fd35fe1b03d72629da599156ccca51f0dfcc3ed
SerafimArts <nesk@xakep.ru>
2022-03-12 01:48:47 +0300

    Fix docblocks

--- src/Vulkan/HeadersDownloader.php
+++ src/Vulkan/HeadersDownloader.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of GLFW3 Headers package.
+ * This file is part of FFI package.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
